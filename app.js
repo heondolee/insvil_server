2da43526b8fb1497226651c6e27623e93e05edad
heondolee <heondo999@inu.ac.kr>
2024-08-01 19:21:58 +0900

    홈 삭제

--- app.js
+++ app.js
@@ -12,12 +12,10 @@ app.get('/', (req, res) => {
 
 const longRouter = require('./router/long.js');
 const loginRouter = require('./router/login.js');
-const homeRouter = require('./router/home.js');
 const userRouter = require('./router/user.js');
 
 app.use('/long', longRouter);
 app.use('/login', loginRouter);
-app.use('/home', homeRouter);
 app.use('/user', userRouter);
 
 app.listen(port, () => {
