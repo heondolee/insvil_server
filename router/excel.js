a0eee23a68d3bc0a7a6f5d184cb3a6bc6e1bdcf7
heondolee <heondo999@inu.ac.kr>
2024-08-20 22:04:45 +0900

    엑셀 스트리밍

--- router/excel.js
+++ router/excel.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const db = require("../models");
 
 const { Long, Car, User, Customer, Reference, Normal } = db;
+const BATCH_SIZE = 1000; // 한번에 처리할 레코드 수
 
 router.post('/', async (req, res) => {
   try {
@@ -33,16 +34,16 @@ router.post('/', async (req, res) => {
         return res.status(400).send('Invalid model name');
     }
 
-    const records = await Model.findAll();
-
-    if (records.length === 0) {
-      return res.status(404).send('No data available');
-    }
-
     const workbook = new ExcelJS.Workbook();
     const worksheet = workbook.addWorksheet(modelName);
 
-    const columns = Object.keys(records[0].dataValues).map(key => ({
+    // 테이블 컬럼 설정
+    const firstBatch = await Model.findAll({ limit: BATCH_SIZE });
+    if (firstBatch.length === 0) {
+      return res.status(404).send('No data available');
+    }
+
+    const columns = Object.keys(firstBatch[0].dataValues).map(key => ({
       header: key.charAt(0).toUpperCase() + key.slice(1),
       key,
       width: 50,
@@ -50,9 +51,20 @@ router.post('/', async (req, res) => {
 
     worksheet.columns = columns;
 
-    records.forEach(record => {
-      worksheet.addRow(record.dataValues);
-    });
+    let offset = 0;
+    let hasMoreData = true;
+
+    while (hasMoreData) {
+      const records = await Model.findAll({ limit: BATCH_SIZE, offset });
+      if (records.length === 0) {
+        hasMoreData = false;
+      } else {
+        records.forEach(record => {
+          worksheet.addRow(record.dataValues);
+        });
+        offset += BATCH_SIZE;
+      }
+    }
 
     res.setHeader(
       'Content-Type',
@@ -70,5 +82,6 @@ router.post('/', async (req, res) => {
     res.status(500).send('Error generating the excel file');
   }
 });
+
 
 module.exports = router;
