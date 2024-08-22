2a75274fdfb3e883f1ad6e30a3f9b02fff06bcb7
heondolee <heondo999@inu.ac.kr>
2024-08-22 15:39:31 +0900

    고객 검사 완료

--- router/customer.js
+++ router/customer.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 
+const { Op } = require("sequelize");
 const { Customer } = db;
 
 router.get("/list", async (req, res) => {
@@ -20,7 +21,9 @@ router.post("/list", async (req, res) => {
   try {
     const customers = await Customer.findAll({
       where: {
-        customerName: customerName,
+        customerName: {
+          [Op.like]: `%${customerName}%`,
+        },
       },
     });
     res.status(200).json(customers);
@@ -37,6 +40,34 @@ router.post("/detail", async (req, res) => {
     const customer = await Customer.findOne({
       where: {
         customerName: customerName,
+      },
+    });
+    res.status(200).json(customer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "서버 오류가 발생했습니다." });
+  }
+});
+
+router.post("/create", async (req, res) => {
+  const customerData = req.body;
+  console.log(customerData);
+  
+  try {
+    const customer = await Customer.create(customerData);
+    res.status(200).json(customer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "서버 오류가 발생했습니다." });
+  }
+});
+
+router.post("/update", async (req, res) => {
+  const customerData = req.body;
+  try {
+    const customer = await Customer.update(customerData, {
+      where: {
+        id: customerData.id,
       },
     });
     res.status(200).json(customer);
